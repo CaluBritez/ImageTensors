ee94fdff03d54a7821917073ad993a5e249d38db
CaluBritez <lucas_britez@hotmail.com>
2024-06-26 12:41:06 -0300

    trabajo con estilos

--- main.js
+++ main.js
@@ -5,22 +5,20 @@ document.getElementById('imageUpload').addEventListener('change', function (e) {
         imgElement.src = event.target.result;
 
         imgElement.onload = async function () {
+            // Muestra el contenedor de imágenes
+            document.getElementById('imageContainer').classList.remove('hidden');
 
             const tensor = tf.browser.fromPixels(imgElement);
-
 
             const resizedBilinear = tf.image.resizeBilinear(tensor, [100, 100]);
             const resizedBilinearNormalized = resizedBilinear.div(tf.scalar(255)); // Normalizar los valores de píxeles
             await tf.browser.toPixels(resizedBilinearNormalized, document.getElementById('resizedImageBilinear'));
 
-
             const resizedNearestNeighbor = tf.image.resizeNearestNeighbor(tensor, [100, 100]);
             await tf.browser.toPixels(resizedNearestNeighbor, document.getElementById('resizedImageNearestNeighbor'));
 
-
             const mirrored = tensor.reverse(1);
             tf.browser.toPixels(mirrored, document.getElementById('mirroredImage'));
-
 
             tensor.dispose();
             resizedBilinear.dispose();
